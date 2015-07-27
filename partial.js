ba0d7f8b49704d0f50658e8aa34278ab7c91e38b
Prakash Sanker <psanker@stanford.edu>
2015-07-26 17:54:58 -0700

    add test cases for partial, got reimplementation with _

--- partial.js
+++ partial.js
@@ -6,14 +6,39 @@ but left open to supply at call-time.
 function partial(func) {
 	var context = this;
 	var originalArgs = Array.prototype.slice.call(arguments);
-	var supplied = originalArgs.shift(); //don't want the original func
+	var supplied = originalArgs.shift(); //don't want the original func //(_, 9 , 3)
 	var result;
 	return function() {
-		var args = Array.prototype.slice.call(arguments);
-		var numberOfSuppliedArguments = 
-
-
-		var newArgs = supplied.concat(args);
+		var args = Array.prototype.slice.call(arguments); //[1, 3]
+		var newArgs = [];
+		for(var i = 0; i < originalArgs.length; i++) {
+				if (originalArgs[i] === "_") {
+					newArgs[i] = args.shift(); //[3]
+				} else {
+					newArgs[i] = originalArgs[i]; //[1, 9, 3 ]
+				}
+		}
+		newArgs = newArgs.concat(args);
 		return func.apply(context, newArgs);
 	}
 }
+
+//test cases
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function subtract2(a, b, c, d) {
+    return a - b - c - d;
+}
+
+var subtest = partial(subtract2, "_", 9, 3);
+subtest(1,3);
+
+
+subtest = partial(subtract2, "_", "_", "_", "_");
+subtest(1,1,1,1);
+
+subtest = partial(subtract2);
+subtest(1,1,1,1);
